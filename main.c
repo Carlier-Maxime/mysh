4d63437122e5da2a8ccdd2119afd9a4cbf480837
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2023-10-10 23:05:29 +0200

    read line and print
    add prefix prompt : '~>'
    Ctrl-D (EOF) for quit

--- main.c
+++ main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main( )
+unsigned int line_size=64, line_pos=0;
+
+void execute_line(char *line) {
+    printf("%s\n~> ", line);
+    line_pos=0;
+}
+
+int main()
 {
-    printf("Hello, World!\n");
-    return 0;
+    int status=EXIT_SUCCESS, c;
+    char *line=malloc(sizeof(char)*line_size), *tmp;
+    if (!line) {
+        status=EXIT_FAILURE;
+        goto exit;
+    }
+    printf("~> ");
+    while ((c=getchar())!=EOF) {
+        if (line_pos==line_size) {
+            tmp=realloc(line, line_size=line_size<1);
+            if (!tmp) {
+                status=EXIT_FAILURE;
+                break;
+            }
+            line=tmp;
+        }
+        line[line_pos++]=(char) (c=='\n' ? '\0' : c);
+        if (c=='\n') execute_line(line);
+    }
+exit:
+    free(line);
+    printf("\nexit mysh with status : %d\n", status);
+    return status;
 }

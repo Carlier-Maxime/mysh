d96b4b005a357bea9c4ecb34dc99641a22a11c17
Carlier Maxime <maxime_carlier@ens.univ-artois.fr>
2023-10-11 11:31:22 +0200

    use enumeration for status

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+typedef enum {
+    NO_ERROR=0,
+    ALLOCATION_FAILED=1
+} Status;
+Status status=NO_ERROR;
+
 typedef struct {
     unsigned int size, pos;
     char *chars;
@@ -9,14 +15,19 @@ typedef struct {
 
 Line *createLine() {
     Line *line=malloc(sizeof(Line));
-    if (!line) return NULL;
+    if (!line) {
+        status=ALLOCATION_FAILED;
+        return NULL;
+    }
     line->pos=0;
     line->size=64;
     line->chars=malloc(sizeof(char)*line->size);
     if (!line->chars) {
         free(line);
+        status=ALLOCATION_FAILED;
         return NULL;
     }
+    status=NO_ERROR;
     return line;
 }
 
@@ -24,10 +35,14 @@ bool Line_addChar(Line *line, char c) {
     if (line->pos==line->size) {
         char* tmp;
         tmp=realloc(line->chars, line->size<<=1);
-        if (!tmp) return false;
+        if (!tmp) {
+            status = ALLOCATION_FAILED;
+            return false;
+        }
         line->chars=tmp;
     }
     line->chars[line->pos++]=(char)(c=='\n' ? '\0' : c);
+    status=NO_ERROR;
     return true;
 }
 
@@ -38,18 +53,12 @@ void execute_line(Line* line) {
 
 int main()
 {
-    int status=EXIT_SUCCESS, c;
+    int c;
     Line* line = createLine();
-    if (!line) {
-        status=EXIT_FAILURE;
-        goto exit;
-    }
+    if (!line) goto exit;
     printf("~> ");
     while ((c=getchar())!=EOF) {
-        if (!Line_addChar(line, (char) c)) {
-            status=EXIT_FAILURE;
-            goto exit;
-        }
+        if (!Line_addChar(line, (char) c)) goto exit;
         if (c=='\n') execute_line(line);
     }
 exit:

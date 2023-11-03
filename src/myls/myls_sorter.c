67616ec43c7343a7f793da365ee2ff674d740921
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2023-11-03 23:58:40 +0100

    change strcasecmp to strcmp

--- src/myls/myls_sorter.c
+++ src/myls/myls_sorter.c
@@ -8,7 +8,7 @@ void sort_dir_path_tab(char** dir_path_tab, int size){
 	for(int i=0;i<size;i++){
 		int min_idx=i;
 		for(int j=i+1;j<size;j++){
-			if(strcasecmp(get_name(dir_path_tab[min_idx]),get_name(dir_path_tab[j]) )>0) min_idx=j;
+			if(strcmp(get_name(dir_path_tab[min_idx]),get_name(dir_path_tab[j]) )>0) min_idx=j;
 		}
 		if(i!=min_idx){
 			char* tmp = dir_path_tab[i];
@@ -23,7 +23,7 @@ void sort_file_tab(full_file* file_tab, int size){
 	for(int i=0;i<size;i++){
 		int min_idx=i;
 		for(int j=i+1;j<size;j++){
-			if(strcasecmp(get_name(file_tab[min_idx].name),get_name(file_tab[j].name))>0) min_idx=j;
+			if(strcmp(get_name(file_tab[min_idx].name),get_name(file_tab[j].name))>0) min_idx=j;
 		}
 		if(i!=min_idx){
 			full_file tmp = file_tab[i];

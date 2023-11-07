19881d259aa7a6db607ac7f868d78e5845588fd9
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2023-11-07 23:02:46 +0100

    add func TokenMapper_processDoubleChar and attribute current_token

--- src/mysh/TokenMapper.c
+++ src/mysh/TokenMapper.c
@@ -11,6 +11,7 @@ TokenMapper* TokenMapper_create() {
     this->last_char=EOF;
     this->last_token=TOKEN_NONE;
     this->current_char=EOF;
+    this->current_token=TOKEN_NONE;
     this->processCurrentChar=false;
     this->escapeChar=false;
     this->buildArg=false;
@@ -32,84 +33,71 @@ bool TokenMapper_setCurrentChar(TokenMapper* this, char c) {
     return true;
 }
 
+bool TokenMapper_processDoubleChar(TokenMapper* this, char c, Token token_double, Token token_single) {
+    if (this->last_char!=c) return false;
+    if (this->last_token==TOKEN_NONE) this->current_token = this->current_char==c ? token_double : token_single;
+    else if (this->current_char==c) this->current_token = TOKEN_NONE;
+    else return false;
+    return true;
+}
+
 Token TokenMapper_process(TokenMapper* this) {
     if (!this) {
         Error_SetError(ERROR_NULL_POINTER);
         return TOKEN_ERROR;
     }
-    Token token;
-    start_if:
-    if (!this->processCurrentChar) token=TOKEN_NONE;
+    if (!this->processCurrentChar) this->current_token=TOKEN_NONE;
     else if (this->last_char=='\\' && this->escapeChar) {
-        token = this->current_char=='\n' ? TOKEN_NEW_LINE : TOKEN_CHAR;
+        this->current_token = this->current_char=='\n' ? TOKEN_NEW_LINE : TOKEN_CHAR;
         this->escapeChar = false;
         this->processCurrentChar=false;
         this->current_char=EOF;
     }
-    else if (this->last_char=='&') {
-        if (this->last_token==TOKEN_NONE) token = this->current_char=='&' ? TOKEN_AND : TOKEN_BACKGROUND;
-        else if (this->current_char=='&') token = TOKEN_NONE;
-        else {
-            this->last_char=EOF;
-            goto start_if;
-        }
-    }
-    else if (this->last_char=='|') {
-        if (this->last_token==TOKEN_NONE) token = this->current_char=='|' ? TOKEN_OR : TOKEN_PIPE;
-        else if (this->current_char=='|') token = TOKEN_NONE;
-        else {
-            this->last_char=EOF;
-            goto start_if;
-        }
-    }
-    else if (this->last_char=='>') {
-        if (this->last_token==TOKEN_NONE) token = this->current_char=='>' ? TOKEN_REDIRECT_OUTPUT_APPEND : TOKEN_REDIRECT_OUTPUT;
-        else if (this->current_char=='>') token = TOKEN_NONE;
-        else {
-            this->last_char=EOF;
-            goto start_if;
-        }
-    }
+    else if (TokenMapper_processDoubleChar(this, '&', TOKEN_AND, TOKEN_BACKGROUND) ||
+            TokenMapper_processDoubleChar(this, '|', TOKEN_OR, TOKEN_PIPE) ||
+            TokenMapper_processDoubleChar(this, '>', TOKEN_REDIRECT_OUTPUT_APPEND, TOKEN_REDIRECT_OUTPUT)
+    ) goto end;
     else if (this->current_char=='\\') {
         this->escapeChar = true;
-        token=TOKEN_NONE;
+        this->current_token=TOKEN_NONE;
     }
     else if (this->current_char=='\n') {
-        if (this->buildArg) token=TOKEN_STR;
-        else if (this->last_token==TOKEN_NONE || this->last_token==TOKEN_STR) token=TOKEN_COMMAND;
-        else if (this->last_token==TOKEN_COMMAND) token=TOKEN_EXECUTE;
-        else token=TOKEN_NONE;
+        if (this->buildArg) this->current_token=TOKEN_STR;
+        else if (this->last_token==TOKEN_NONE || this->last_token==TOKEN_STR) this->current_token=TOKEN_COMMAND;
+        else if (this->last_token==TOKEN_COMMAND) this->current_token=TOKEN_EXECUTE;
+        else this->current_token=TOKEN_NONE;
     }
     else if (IS_WHITE_SPACE(this->current_char)) {
         this->processCurrentChar = false;
-        token = IS_WHITE_SPACE(this->last_char) ? TOKEN_NONE : TOKEN_STR;
+        this->current_token = IS_WHITE_SPACE(this->last_char) ? TOKEN_NONE : TOKEN_STR;
     }
     else if(this->current_char==';') {
-        if (this->buildArg) token=TOKEN_STR;
+        if (this->buildArg) this->current_token=TOKEN_STR;
         else {
-            token = TOKEN_COMMAND;
+            this->current_token = TOKEN_COMMAND;
             this->processCurrentChar=false;
         }
     }
     else if (this->current_char=='&' || this->current_char=='|' || this->current_char=='>') {
-        if (this->buildArg) token=TOKEN_STR;
-        else token = TOKEN_NONE;
+        if (this->buildArg) this->current_token=TOKEN_STR;
+        else this->current_token = TOKEN_NONE;
     }
     else if (this->current_char=='<') {
-        if (this->buildArg) token=TOKEN_STR;
+        if (this->buildArg) this->current_token=TOKEN_STR;
         else {
-            token = TOKEN_REDIRECT_INPUT;
+            this->current_token = TOKEN_REDIRECT_INPUT;
             this->processCurrentChar=false;
         }
     }
     else {
-        token=TOKEN_CHAR;
+        this->current_token=TOKEN_CHAR;
         this->processCurrentChar=false;
     }
-    if (token==TOKEN_NONE) this->processCurrentChar=false;
-    else if (token==TOKEN_CHAR) this->buildArg=true;
-    else if (token==TOKEN_STR) this->buildArg=false;
-    this->last_token=token;
+    end:
+    if (this->current_token==TOKEN_NONE) this->processCurrentChar=false;
+    else if (this->current_token==TOKEN_CHAR) this->buildArg=true;
+    else if (this->current_token==TOKEN_STR) this->buildArg=false;
+    this->last_token=this->current_token;
     Error_SetError(ERROR_NONE);
-    return token;
+    return this->current_token;
 }
--- src/mysh/TokenMapper.h
+++ src/mysh/TokenMapper.h
@@ -23,7 +23,7 @@ typedef enum {
 typedef struct TokenMapper {
     bool processCurrentChar, escapeChar, buildArg;
     char last_char, current_char;
-    Token last_token;
+    Token last_token, current_token;
 } TokenMapper;
 
 TokenMapper* TokenMapper_create();
